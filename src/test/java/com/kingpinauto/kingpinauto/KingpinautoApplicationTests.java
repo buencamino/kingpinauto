9065f52ed396831e6c2395ec840d6017bea62f82
tempest <ml.francis@yahoo.com>
2024-07-03 15:07:08 +0800

    first commit

---
+++ src/test/java/com/kingpinauto/kingpinauto/KingpinautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingpinauto.kingpinauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingpinautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
